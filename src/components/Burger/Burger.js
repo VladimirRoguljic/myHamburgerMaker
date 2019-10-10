3bf73700c14b1f035f6f046a08d8a89e6ced72c2
Vladimir Roguljic <vladimir.roguljic@gauss.hr>
2019-10-10 11:16:44 +0200

    outputing multiple build controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-    const transformIngredients = Object.keys(props.ingredients)
+    let transformIngredients = Object.keys(props.ingredients)
         .map(iKey => {
             return [...Array(props.ingredients[iKey])]
                 .map((_, i) => {
                return <BurgerIngredient key={iKey + i} type={iKey} />
             });
-        });
+        }).reduce((arr, el) => {
+             return arr.concat(el)
+        }, []);
+
+    if(transformIngredients.length === 0) {
+        transformIngredients = <p>Please start adding ingredients </p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
