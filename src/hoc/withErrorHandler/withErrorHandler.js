ad68d237e8190efce66979bce646d315c1071461
Vladimir Roguljic <vladimir.roguljic@gauss.hr>
2019-10-14 09:57:28 +0200

    onmount interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,13 +9,18 @@ const withErrorHandler = (WrappedComponent, axios) => {
         };
 
         componentWillMount() {
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             })
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.request.eject(this.resInterceptor)
         }
 
         errorConfirmedHandler = () => {
@@ -28,7 +33,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
                     <Modal
                         show={this.state.error}
                         modalClosed={this.errorConfirmedHandler}>
-                        {this.state.error ? this.state.error.message: null}
+                        {this.state.error ? this.state.error.message : null}
                     </Modal>
                     <WrappedComponent {...this.props}/>
                 </Auxiliary>
