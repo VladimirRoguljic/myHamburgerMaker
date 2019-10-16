3376a3fd8bd33d4a8f7549a09e1f93c9a1b1837d
Vladimir Roguljic <vladimir.roguljic@gauss.hr>
2019-10-16 09:06:35 +0200

    adding order sumbission and passing data between pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,30 +90,11 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Vladimir Roguljić',
-        //         address: {
-        //             street: 'Test street',
-        //             zipCode: '11222',
-        //             country: 'Croatia'
-        //         },
-        //         email: 'test@test.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // };
-        // axios.post('/orders.json', order).then(response => {
-        //     this.setState({loading: false, purchasing: false});
-        // }).catch(error => {
-        //     this.setState({loading: false, purchasing: false});
-        // })
         const queryParams = [];
         for(let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
