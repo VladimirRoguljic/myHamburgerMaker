50df086a6acc09a23245438ea3cf84fbc3376b17
Vladimir Roguljic <vladimir.roguljic@gauss.hr>
2019-10-17 12:57:22 +0200

    adding custom validators form form

--- src/containers/Checkoutt/ContactData/ContactData.js
+++ src/containers/Checkoutt/ContactData/ContactData.js
@@ -15,7 +15,10 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Your Name'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                }
             },
             street: {
                 elementType: 'input',
@@ -23,7 +26,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             zipCode: {
                 elementType: 'input',
@@ -31,7 +38,13 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'ZIP Code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false
             },
             country: {
                 elementType: 'input',
@@ -39,7 +52,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             email: {
                 elementType: 'input',
@@ -47,15 +64,19 @@ class ContactData extends Component {
                     type: 'email',
                     placeholder: 'Your Mail'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             deliveryMethod: {
                 elementType: 'select',
                 elementConfig: {
                     options: [
-                        {value: 'fastest', displayValue:'Fastest'},
-                        {value: 'cheapest', displayValue:'Cheapest'},
-                        ]
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'},
+                    ]
                 },
                 value: ''
             }
@@ -84,15 +105,36 @@ class ContactData extends Component {
         })
     };
 
+    checkValidity(value, rules) {
+        let isValid = true;
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.minLength && isValid
+        }
+
+        return isValid
+    }
+
     inputChangedHandler = (event, inputIdentifier) => {
-         const updateOrderForm = {
-             ...this.state.orderForm
-         };
+        const updateOrderForm = {
+            ...this.state.orderForm
+        };
         const updateFormElement = {
-             ...updateOrderForm[inputIdentifier]
+            ...updateOrderForm[inputIdentifier]
         };
         updateFormElement.value = event.target.value;
+        updateFormElement.valid = this.checkValidity(updateFormElement.value, updateFormElement.validation);
         updateOrderForm[inputIdentifier] = updateFormElement;
+        console.log(updateFormElement)
         this.setState({orderForm: updateOrderForm})
     };
 
@@ -112,7 +154,7 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
-                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
