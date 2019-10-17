95a569034bb9d4ade33f53ee934d1c30326014e1
Vladimir Roguljic <vladimir.roguljic@gauss.hr>
2019-10-17 09:24:39 +0200

    creating custom dynamic input component

--- src/containers/Checkoutt/ContactData/ContactData.js
+++ src/containers/Checkoutt/ContactData/ContactData.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import Button from '../../../components/UI/Button/Button';
-import Spinner from  '../../../components/UI/Spinner/Spinner';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-order';
+import Input from '../../../components/UI/Input/Input'
 
 
 class ContactData extends Component {
@@ -46,16 +47,16 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="name" placeholder="Your Name"/>
-                <input className={classes.Input} type="text" name="email" placeholder="Your Mail"/>
-                <input className={classes.Input} type="text" name="street" placeholder="Street"/>
-                <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"/>
+                <Input  inputtype="input"  type="text" name="name" placeholder="Your Name"/>
+                <Input  inputtype="input" type="text" name="email" placeholder="Your Mail"/>
+                <Input  inputtype="input" type="text" name="street" placeholder="Street"/>
+                <Input inputtype="input"  type="text" name="postal" placeholder="Postal Code"/>
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
 
         );
         if (this.state.loading) {
-            form = <Spinner />
+            form = <Spinner/>
         }
         return (
             <div className={classes.ContactData}>
