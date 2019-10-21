b2aacdeb483c223adcdff782e1fd7b051854e880
Vladimir Roguljic <vladimir.roguljic@gauss.hr>
2019-10-22 00:23:56 +0200

    getting a token from backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionsTypes';
+import axios from 'axios'
 
 export const authStart = () => {
     return  {
@@ -20,6 +21,20 @@ export const authFail = (error) => {
 
 export const auth = (email,password) => {
     return dispatch => {
-        dispatch(authStart())
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC_N8FLceIK9Rqy9gcMZ19fxGIHfYd1-5g', authData )
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data))
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err))
+            })
     }
 };
