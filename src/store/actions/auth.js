cc7fc3ae50607d72913f251896c2af0ddc2228b4
Vladimir Roguljic <vladimir.roguljic@gauss.hr>
2019-10-22 11:06:59 +0200

    logging users out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,21 @@ export const authFail = (error) => {
     }
 };
 
+
+export const checkAuthTimeout = (expirationTime)  => {
+    return dispatch => {
+        setTimeout(() => {
+          dispatch(logout())
+        }, expirationTime * 1000)
+    }
+};
+
+export  const logout = () => {
+    return  {
+       type: actionTypes.AUTH_LOGOUT
+    }
+};
+
 export const auth = (email,password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
@@ -38,11 +53,10 @@ export const auth = (email,password, isSignup) => {
 
         axios.post(url, authData )
             .then(response => {
-                console.log(response);
-                dispatch(authSuccess(response.data.idToken, response.data.localId))
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(err => {
-                console.log(err);
                 dispatch(authFail(err.response.data.error))
             })
     }
