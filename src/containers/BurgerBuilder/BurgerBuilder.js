dff031e73c4959837af23d6190ac49db191b2789
Vladimir Roguljic <vroguljic1@gmail.com>
2019-10-25 13:07:52 +0200

    testing burberbuilder containers

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,7 @@ import axios from '../../axios-order';
 
 
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
     state = {
         purchasing: false,
     };
